f5e5cf95f063369c7ad0b8acd808c00c7e41aa95
ehsanmousavif <ehsan.mousavi.f12345@gmail.com>
2025-09-28 02:09:56 +0330

    c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ export default function Home() {
       <Banner />
       <div className="hidden md:block">
         <Benefits />
+        <Benefits />
+        <Benefits />
       </div>
     </section>
   );
