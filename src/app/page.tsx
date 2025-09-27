8191e7f26c853e3f26e641c377f5aeae48bb2849
ehsanmousavif <ehsan.mousavi.f12345@gmail.com>
2025-09-28 01:26:56 +0330

    feat(benefit):the page about benfits and abilitis created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+"use client";
 import Banner from "@/components/banner";
+import { Benefits } from "@/components/benefits";
 
 export default function Home() {
   return (
-    <section className="w-full flex flex-col">
+    <section className="w-full flex flex-col relative">
       <Banner />
+      <div className="hidden md:block">
+        <Benefits />
+      </div>
     </section>
   );
 }
