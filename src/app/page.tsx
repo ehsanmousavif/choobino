941601e4e7805e1c19313bb49ebaba6456efbfd0
webzen <webzen.ir@gmail.com>
2025-09-28 18:45:10 +0330

    add blog section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Banner from "@/components/banner";
+import Blogs from "@/components/Blogs";
 
 export default function Home() {
   return (
-    <section className="w-full flex flex-col">
+    <section className="w-full flex flex-col font-sans">
       <Banner />
+      <Blogs />
     </section>
   );
 }
