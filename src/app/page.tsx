0a80e0250c288d04840263e9b4972ad8be1455fc
ehsanmousavif <ehsan.mousavi.f12345@gmail.com>
2025-09-29 16:51:24 +0330

    feat(benefits):created a page for benefits and abilits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Banner from "@/components/banner";
+import Benefits from "@/components/benefits";
 
 export default function Home() {
   return (
     <section className="w-full flex flex-col">
       <Banner />
+      <div className=" hidden md:block w-full pt-6">
+        {" "}
+        <Benefits />
+      </div>
     </section>
   );
 }
---
+++ src/components/benefits.tsx
@@ -0,0 +1,71 @@
+import {
+  CheckBadgeIcon,
+  TruckIcon,
+  ReceiptPercentIcon,
+  PencilSquareIcon,
+} from "@heroicons/react/24/outline";
+
+import React from "react";
+
+interface BenefitTypes {
+  title: string;
+  description: string;
+  Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+}
+
+const BENEFIT_TEXTS: Record<string, BenefitTypes> = {
+  guarantee: {
+    title: "تضمین ضمانت اصل کالا",
+    description:
+      "تمامی محصولات با ضمانت اصل بودن و کیفیت عالی عرضه می‌شوند تا با خیالی آسوده خرید کنید.",
+    Icon: CheckBadgeIcon,
+  },
+  delivery: {
+    title: "ارسال به سراسر کشور",
+    description:
+      "هر جایی از ایران که باشید، سفارش شما با بسته‌بندی مطمئن و در کوتاه‌ترین زمان به دستتان خواهد رسید.",
+    Icon: TruckIcon,
+  },
+  payment: {
+    title: "پرداخت آنلاین",
+    description:
+      "امکان پرداخت اینترنتی از درگاه‌های امن فراهم است تا تجربه‌ای راحت و بدون دغدغه داشته باشید.",
+    Icon: ReceiptPercentIcon,
+  },
+  customization: {
+    title: "سفارشی سازی محصول",
+    description:
+      "هر محصول قابلیت طراحی و ساخت اختصاصی دارد تا کاملاً متناسب با سلیقه و نیاز شما ساخته شود.",
+    Icon: PencilSquareIcon,
+  },
+};
+
+function BenefitCard({ title, description, Icon }: BenefitTypes) {
+  return (
+    <div className="flex flex-col items-start gap-4 p-4">
+      <div className="w-fit h-fit bg-secondary-500/10 p-5 text-secondary-400 rounded-full">
+        <Icon className="size-10" aria-label={title} />
+      </div>
+      <div className="flex flex-col gap-2 items-start">
+        <span className="text-xl text-secondary mt-6">-</span>
+
+        <span className="text-xl font-bold">{title}</span>
+        <p className="text-sm text-foreground-800 font-light line-clamp-3">
+          {description}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default function Benefits() {
+  return (
+    <section className="w-full py-16">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        {Object.values(BENEFIT_TEXTS).map((benefit) => (
+          <BenefitCard key={benefit.title} {...benefit} />
+        ))}
+      </div>
+    </section>
+  );
+}
