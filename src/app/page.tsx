63bcff8bdaed75192bf692b28b2874e351631c7e
ehsanmousavif <ehsan.mousavi.f12345@gmail.com>
2025-10-12 17:42:08 +0330

    c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 "use client";
 import Banner from "@/components/banner";
-import { CustomProduct } from "@/components/custom-product";
+import { Benefits } from "@/components/benefits";
 import Blogs from "@/components/Blogs";
 import Comments from "@/components/comments-components/comments";
 import TopSales from "@/components/top-sales";
 
 export default function Home() {
   return (
-    <section className="w-full flex flex-col relative">
+    <section className="w-full flex flex-col relative gap-12">
       <Banner />
+      <Benefits />
       <TopSales />
+      <Blogs />
+      <Comments />
     </section>
   );
 }
